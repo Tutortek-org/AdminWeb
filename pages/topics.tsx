674eb44c2700b1ba59d19e1fcdb68f7d7af8d655
Zablas <vagabaliukas@gmail.com>
2022-02-20 15:08:26 +0200

    Add topic retrieval from API

--- pages/topics.tsx
+++ pages/topics.tsx
@@ -1,4 +1,7 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
 import SimpleLayout from "../components/layout/simple";
+import { Topic } from "../interfaces/topic";
 
 export default function Topics() {
   return (
@@ -7,3 +10,35 @@ export default function Topics() {
     </SimpleLayout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/topics/unapproved`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${session?.accessToken}`,
+      },
+    }
+  );
+
+  var topics: Array<Topic> = [];
+  var isErrorPresent: Boolean = false;
+  try {
+    if (res.ok) {
+      topics = await res.json();
+      console.log(topics);
+    } else {
+      isErrorPresent = true;
+    }
+  } catch (e) {
+    isErrorPresent = true;
+  }
+
+  return {
+    props: { topics, isErrorPresent },
+  };
+};
